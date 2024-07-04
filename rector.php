d8a948a4d2d03d459a66cf7063393495ab9a03de
Alex Diaz <alex.diaz@540deg.com>
2024-07-04 12:08:10 +0200

    [2] - Configurar grumphp

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/laravel',
+        __DIR__ . '/public',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
