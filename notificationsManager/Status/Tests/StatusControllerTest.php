59b992f7b7ec2e34f370a198eb79c6eeed79de03
Alex Diaz <alex.diaz@540deg.com>
2024-07-04 12:41:12 +0200

    [3] - Configurar CI

--- notificationsManager/Status/Tests/StatusControllerTest.php
+++ notificationsManager/Status/Tests/StatusControllerTest.php
@@ -2,13 +2,15 @@
 
 namespace NotificationsManager\Status\Tests;
 
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
+#[Group("Integration")]
 class StatusControllerTest extends TestCase
 {
     #[Test]
-    public function checkStatusOk()
+    public function checkStatusOk(): void
     {
         $expectedResponse = '{"status":"ok"}';
 
