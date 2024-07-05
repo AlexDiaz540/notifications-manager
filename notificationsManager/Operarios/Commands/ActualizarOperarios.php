dc379e9412a60777b77213bfd8cb7682feea0845
Alex Diaz <alex.diaz@540deg.com>
2024-07-05 12:17:28 +0200

    [4] - Devolver toda la información con test en rojo

--- notificationsManager/Operarios/Commands/ActualizarOperarios.php
+++ notificationsManager/Operarios/Commands/ActualizarOperarios.php
@@ -2,6 +2,7 @@
 
 namespace NotificationsManager\Operarios\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http as HttpClient;
 
@@ -22,19 +23,37 @@ class ActualizarOperarios extends Command
     public function handle(): void
     {
         $url = 'https://api.extexnal.com/operators/?sequence_number=12341234';
+        $data = [
+            [
+                'sequenceNumber' => 1510105,
+                'journalEntryType' => 'UP',
+                'customerId' => 26,
+                'id' => 2,
+                'name' => '654654',
+                'surname1' => '',
+                'surname2' => '',
+                'phone' => 0,
+                'email' => '',
+                'orderNotifications' => false,
+                'orderNotificationEmail' => '',
+                'orderNotificationByEmail' => false,
+                'orderNotificationBySms' => false,
+                'orderNotificationByPush' => false,
+                'deleted' => true,
+                'object' => 'SALQ9U',
+                'objectSchema' => 'IQSFCOMUN'
+            ]
+        ];
 
         $this->httpClient::fake([
-            $url => $this->httpClient::response([
-                [
-                    'sequenceNumber' => '12341234',
-                ]
-            ], 200)
+            $url => $this->httpClient::response($data, 200)
         ]);
 
         $response = $this->httpClient::get($url);
         $operatorsInfo = $response->json();
-        foreach ($operatorsInfo as $operator) {
-            $this->info("Sequence Number: " . $operator['sequenceNumber']);
+        try {
+            $this->line(json_encode($operatorsInfo, JSON_THROW_ON_ERROR));
+        } catch (Exception $e) {
         }
     }
 }
