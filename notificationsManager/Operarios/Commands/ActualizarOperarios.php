022154b080745dc7bed9a6d31129a8b3b881cdcf
Alex Diaz <alex.diaz@540deg.com>
2024-07-05 11:04:31 +0200

    [4] - Realizar petición con el comando con test en rojo

--- notificationsManager/Operarios/Commands/ActualizarOperarios.php
+++ notificationsManager/Operarios/Commands/ActualizarOperarios.php
@@ -3,15 +3,38 @@
 namespace NotificationsManager\Operarios\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http as HttpClient;
 
 class ActualizarOperarios extends Command
 {
-    protected $signature = 'notifications:actualizarOperarios';
+    protected $signature = 'update:operarios';
 
     protected $description = 'Actualiza los datos de los operarios';
 
+    private HttpClient $httpClient;
+
+    public function __construct(HttpClient $httpClient)
+    {
+        parent::__construct();
+        $this->httpClient = $httpClient;
+    }
+
     public function handle(): void
     {
-        $this->info("Funciona Correctamente");
+        $url = 'https://api.extexnal.com/operators/?sequence_number=12341234';
+
+        $this->httpClient::fake([
+            $url => $this->httpClient::response([
+                [
+                    'sequenceNumber' => '12341234',
+                ]
+            ], 200)
+        ]);
+
+        $response = $this->httpClient::get($url);
+        $operatorsInfo = $response->json();
+        foreach ($operatorsInfo as $operator) {
+            $this->info("Sequence Number: " . $operator['sequenceNumber']);
+        }
     }
 }
