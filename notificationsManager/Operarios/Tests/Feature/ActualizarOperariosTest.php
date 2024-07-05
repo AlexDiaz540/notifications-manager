7524bfd23dd6e8c65170627b9a9ad163f67d5422
Alex Diaz <alex.diaz@540deg.com>
2024-07-05 12:23:31 +0200

    [4] - Devolver toda la información con test en verde

--- notificationsManager/Operarios/Tests/Feature/ActualizarOperariosTest.php
+++ notificationsManager/Operarios/Tests/Feature/ActualizarOperariosTest.php
@@ -10,7 +10,13 @@ class ActualizarOperariosTest extends TestCase
     #[Test]
     public function testActualizarOperarios(): void
     {
+        $expectedResponse = '[{"sequenceNumber":1510105,"journalEntryType":"UP","customerId":26,"id":2,"name":"654654",
+            "surname1":"","surname2":"","phone":0,"email":"","orderNotifications":false,"orderNotificationEmail":"",
+            "orderNotificationByEmail":false,"orderNotificationBySms":false,"orderNotificationByPush":false,
+            "deleted":true,"object":"SALQ9U","objectSchema":"IQSFCOMUN"}]';
+
+        $expectedResponse = str_replace(["\r", "\n", "\t", " "], '', $expectedResponse);
         $this->artisan('update:operarios')
-            ->expectsOutput('Sequence Number: 12341234');
+            ->expectsOutput($expectedResponse);
     }
 }
