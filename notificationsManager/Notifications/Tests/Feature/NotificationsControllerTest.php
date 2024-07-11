39c0234dfd4dab2360a37dfb5b5947e06d5e735e
Alex Diaz <alex.diaz@540deg.com>
2024-07-11 13:38:35 +0200

    [4] - Comprobar formato request con test en verde

--- notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
+++ notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
@@ -10,11 +10,32 @@ class NotificationsControllerTest extends TestCase
     #[Test]
     public function notificationsAddTest(): void
     {
-        $expectedResponse = '{"":"ok"}';
+        $expectedResponse = '{"message":"Pending notification added successfully."}';
 
-        $response = $this->post(route("addNotification"));
+        $response = $this->post(route('addNotification'), [
+            'id' => null,
+            'customerId' => '12345',
+            'operatorId' => 678,
+            'orderNumber' => 'ORD123456',
+            'messageType' => 'INFO',
+            'createdDate' => '2024-07-03T10:00:00Z'
+        ]);
 
         $response->assertStatus(200);
+        $response->assertContent($expectedResponse);
+    }
+    #[Test]
+    public function notificationsAddTestWithBadFormatRequest(): void
+    {
+        $expectedResponse = '{"message":"Invalid request format."}';
+
+        $response = $this->post(route('addNotification'), [
+            'id' => null,
+            'customerId' => '12345',
+            'createdDate' => '2024-07-03T10:00:00Z'
+        ]);
+
+        $response->assertStatus(400);
         $response->assertContent($expectedResponse);
     }
 }
