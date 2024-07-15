0f28ba9abcec8db6f2c94e93182d889d74d1d1d3
Alex Diaz <alex.diaz@540deg.com>
2024-07-15 08:43:53 +0200

    [5] - Insertar notificación en la base ded datos con test en verde

--- notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
+++ notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
@@ -23,11 +23,19 @@ class NotificationsControllerTest extends TestCase
             'messageType' => 'INFO',
             'createdDate' => '2024-07-03T10:00:00Z'
         ];
+        $databaseData = [
+            'id' => 1,
+            'customer_id' => '12345',
+            'operator_id' => 678,
+            'order_number' => 'ORD123456',
+            'message_type' => 'INFO',
+            'created_date' => '2024-07-03 10:00:00'
+        ];
         $response = $this->post(route('addNotification'), $notificationData);
 
         $response->assertStatus(200);
         $response->assertContent($expectedResponse);
-        $this->assertDatabaseHas('pending_notifications', $notificationData);
+        $this->assertDatabaseHas('pending_notifications', $databaseData);
     }
     #[Test]
     public function notificationsAddTestWithBadFormatRequest(): void
