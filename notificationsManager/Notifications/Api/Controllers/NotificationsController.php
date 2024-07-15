3221331db4fea29d2fd1eaf7b4e3f9e2d4269b36
Alex Diaz <alex.diaz@540deg.com>
2024-07-15 10:25:49 +0200

    [5] - Comprobar inserción en base de datos con test en rojo

--- notificationsManager/Notifications/Api/Controllers/NotificationsController.php
+++ notificationsManager/Notifications/Api/Controllers/NotificationsController.php
@@ -3,6 +3,8 @@
 namespace NotificationsManager\Notifications\Api\Controllers;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use NotificationsManager\Notifications\Database\Entities\PendingNotification;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
@@ -11,22 +13,32 @@ use NotificationsManager\Notifications\Api\Requests\NotificationAddRequest;
 class NotificationsController
 {
     private EntityManagerInterface $entityManager;
+    private PendingNotification $pendingNotification;
 
     /**
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, PendingNotification $pendingNotification)
     {
         $this->entityManager = $entityManager;
+        $this->pendingNotification = $pendingNotification;
     }
 
     public function __invoke(NotificationAddRequest $request): JsonResponse
     {
-        $pendingNotificationData = $request->all();
-        $pendingNotification = new PendingNotification();
-        $pendingNotification->setPendingNotification($pendingNotificationData);
-        $this->entityManager->persist($pendingNotification);
-        $this->entityManager->flush();
+        try {
+            $pendingNotificationData = $request->all();
+            $this->pendingNotification->setPendingNotification($pendingNotificationData);
+            $this->entityManager->persist($this->pendingNotification);
+            $this->entityManager->flush();
+        } catch (Exception $exception) {
+            throw new HttpResponseException(response()->json(
+                ['message' => "Failed to add pending notification."],
+                500,
+                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+                JSON_UNESCAPED_UNICODE
+            ));
+        }
 
         return response()->json([
             'message' => 'Pending notification added successfully.',
--- notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
+++ notificationsManager/Notifications/Tests/Feature/NotificationsControllerTest.php
@@ -2,7 +2,12 @@
 
 namespace NotificationsManager\Notifications\Tests\Feature;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery\Container;
+use NotificationsManager\Notifications\Api\Controllers\NotificationsController;
+use NotificationsManager\Notifications\Database\Entities\PendingNotification;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -31,12 +36,14 @@ class NotificationsControllerTest extends TestCase
             'message_type' => 'INFO',
             'created_date' => '2024-07-03 10:00:00'
         ];
+
         $response = $this->post(route('addNotification'), $notificationData);
 
         $response->assertStatus(200);
         $response->assertContent($expectedResponse);
         $this->assertDatabaseHas('pending_notifications', $databaseData);
     }
+
     #[Test]
     public function notificationsAddTestWithBadFormatRequest(): void
     {
@@ -49,6 +56,49 @@ class NotificationsControllerTest extends TestCase
         ]);
 
         $response->assertStatus(400);
+        $response->assertContent($expectedResponse);
+    }
+
+    #[Test]
+    public function notificationsAddWithEntityManagerError(): void
+    {
+        $expectedResponse = '{"message":"Failed to add pending notification."}';
+        $notificationData = [
+            'id' => null,
+            'customerId' => '12345',
+            'operatorId' => 678,
+            'orderNumber' => 'ORD123456',
+            'messageType' => 'INFO',
+            'createdDate' => '2024-07-03T10:00:00Z'
+        ];
+
+        $this->mock(EntityManagerInterface::class, function ($mock) {
+            $mock->shouldReceive('persist')->andThrow(new Exception('Database error'));
+            $mock->shouldReceive('flush')->andThrow(new Exception('Database error'));
+        });
+
+        $response = $this->post(route('addNotification'), $notificationData);
+
+        $response->assertStatus(500);
+        $response->assertContent($expectedResponse);
+    }
+
+    #[Test]
+    public function notificationsAddTestWithEntityError(): void
+    {
+        $expectedResponse = '{"message":"Failed to add pending notification."}';
+        $notificationData = [
+            'id' => null,
+            'customerId' => '12345',
+            'operatorId' => 678,
+            'orderNumber' => 'ORD123456',
+            'messageType' => 'INFO',
+            'createdDate' => '2024-07-03T10:00:00Z'
+        ];
+
+        $response = $this->post(route('addNotification'), $notificationData);
+
+        $response->assertStatus(500);
         $response->assertContent($expectedResponse);
     }
 }
